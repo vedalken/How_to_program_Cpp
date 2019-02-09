fc21a4f9464461876859450b825a71c96c3896fe
vedalken <darigaaz@protonmail.com>
2019-02-09 12:56:43 +0100

    Enhance tree class allowing duplicates.

--- How_to_programC++/19.Custom_Templatized_Data_Structure/19.6-Trees/include/tree.h
+++ How_to_programC++/19.Custom_Templatized_Data_Structure/19.6-Trees/include/tree.h
@@ -14,27 +14,27 @@ public:
 
     ~Tree()
     {
-	deleteNodeHelper(&m_rootNode);
+        deleteNodeHelper(&m_rootNode);
     }
 
     void insertNode(const NODETYPE& a_data)
     {
-	insertNodeHelper( &m_rootNode, a_data );
+        insertNodeHelper( &m_rootNode, a_data );
     }
 
     void preOrderTraversal() const
     {
-	preOrderHelper(m_rootNode);
+        preOrderHelper(m_rootNode);
     }
 
     void inOrderTraversal() const
     {
-	inOrderHelper(m_rootNode);
+        inOrderHelper(m_rootNode);
     }
 
     void postOrderTraversal() const
     {
-	postOrderHelper(m_rootNode);
+        postOrderHelper(m_rootNode);
     }
 
 private:
@@ -48,68 +48,64 @@ private:
     // function parameter type as *& (reference to pointer) instead
     // of ** (pointer to pointer or shortly pptr).
     void insertNodeHelper(TreeNode<NODETYPE>** a_ptr,
-			  const NODETYPE&      a_data)
+                          const NODETYPE&      a_data)
     {
-	if (*a_ptr == nullptr) { // derefernce pptr == ptr
-	    // Empty tree node.
-	    *a_ptr = new TreeNode<NODETYPE>(a_data);
-	}
-	else {
-	    if ( a_data < (*a_ptr)->m_data ) {
-		insertNodeHelper( &((*a_ptr)->m_leftNode), a_data );
-	    }
-	    else {
-		if (a_data > (*a_ptr)->m_data) {
-		    insertNodeHelper( &((*a_ptr)->m_rightNode), a_data );
-		}
-		else {
-		    std::cout << a_data << " duplicate" << std::endl;
-		}
-	    }
-	}
+        if (*a_ptr == nullptr) { // derefernce pptr == ptr
+            // Empty tree node.
+            *a_ptr = new TreeNode<NODETYPE>(a_data);
+        }
+        else {
+            // allow duplicate insertion
+            if (a_data <= (*a_ptr)->m_data) {
+                insertNodeHelper( &((*a_ptr)->m_leftNode), a_data );
+            }
+            else {
+                insertNodeHelper( &((*a_ptr)->m_rightNode), a_data );
+            }
+        }
     }
 
     // Recursively delete nodes in postorder traversal of Tree
     void deleteNodeHelper(TreeNode<NODETYPE>** a_ptr)
     {
-	if (*a_ptr != nullptr) {
-	    deleteNodeHelper( &((*a_ptr)->m_leftNode) );
-	    deleteNodeHelper( &((*a_ptr)->m_rightNode) );
-	    // Delete root node.
-	    delete *a_ptr;
-	    *a_ptr = nullptr;
-	    a_ptr = nullptr;
-	}
+        if (*a_ptr != nullptr) {
+            deleteNodeHelper( &((*a_ptr)->m_leftNode) );
+            deleteNodeHelper( &((*a_ptr)->m_rightNode) );
+            // Delete root node.
+            delete *a_ptr;
+            *a_ptr = nullptr;
+            a_ptr = nullptr;
+        }
     }
 
     // Preorder processing tree nodes without tree modifications
     void preOrderHelper(TreeNode<NODETYPE>* a_ptr) const
     {
-	if (a_ptr != nullptr) {
-	    std::cout << a_ptr->m_data << ' ';
-	    preOrderHelper(a_ptr->m_leftNode);
-	    preOrderHelper(a_ptr->m_rightNode);
-	}
+        if (a_ptr != nullptr) {
+            std::cout << a_ptr->m_data << ' ';
+            preOrderHelper(a_ptr->m_leftNode);
+            preOrderHelper(a_ptr->m_rightNode);
+        }
     }
 
     // Inorder processing tree nodes without tree modifications
     void inOrderHelper(TreeNode<NODETYPE>* a_ptr) const
     {
-	if (a_ptr != nullptr) {
-	    inOrderHelper(a_ptr->m_leftNode);
-	    std::cout << a_ptr->m_data << ' ';
-	    inOrderHelper(a_ptr->m_rightNode);
-	}
+        if (a_ptr != nullptr) {
+            inOrderHelper(a_ptr->m_leftNode);
+            std::cout << a_ptr->m_data << ' ';
+            inOrderHelper(a_ptr->m_rightNode);
+        }
     }
 
     // Postorder processing tree nodes without tree modifications
     void postOrderHelper(TreeNode<NODETYPE>* a_ptr) const
     {
-	if (a_ptr != nullptr) {
-	    postOrderHelper(a_ptr->m_leftNode);
-	    postOrderHelper(a_ptr->m_rightNode);
-	    std::cout << a_ptr->m_data << ' ';
-	}
+        if (a_ptr != nullptr) {
+            postOrderHelper(a_ptr->m_leftNode);
+            postOrderHelper(a_ptr->m_rightNode);
+            std::cout << a_ptr->m_data << ' ';
+        }
     }
 };
 
